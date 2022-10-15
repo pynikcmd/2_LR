9b7cb01426a46ecb512959fc1a09f4a6dd661f70
pynikcmd <ver.r762@gmail.com>
2022-10-15 10:27:22 +0300

    Добавлена новая строчка

--- 2_LR/2_LR/2_LR.cpp
+++ 2_LR/2_LR/2_LR.cpp
@@ -1,6 +1,12 @@
 ﻿#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Russian");
+    
+    cout << "Лабораторная работа №2";
+
+
+
 }
